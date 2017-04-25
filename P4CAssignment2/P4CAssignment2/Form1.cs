aa186051842a42abce579f035b003409e1d2b69e
b6002436 <b6002436@my.shu.ac.uk>
2017-04-25 17:19:14 +0100

    Button 9 displays in the multi press textbox

--- P4CAssignment2/P4CAssignment2/Form1.cs
+++ P4CAssignment2/P4CAssignment2/Form1.cs
@@ -66,7 +66,21 @@ namespace P4CAssignment2
 
         private void btn_button9_Click(object sender, EventArgs e)
         {
+            if (firsttime == true)
+            {
+                //Clears the global list box 
+                lb_global.Items.Clear();
 
+                //Adds contents of list box button 9 into the global listbox
+                lb_global.Items.AddRange(lb_button9.Items);
+            }
+
+            // Turns timer off
+            lettertimer.Enabled = false;
+            //Adds one onto the letter number variabe
+            letternumber++;
+            //turns the timer back on
+            lettertimer.Enabled = true; 
         }
 
         private void button1_Click(object sender, EventArgs e)
